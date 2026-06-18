04a759e95fc9e5a98fbe02b8abff956b0c1986b8
timtaran <tentaranka@gmail.com>
2026-06-18 12:44:38 +0500

    Add method to retrieve constraint by UUID in VxConstraintManager

--- common/src/main/java/net/xmx/velthoric/core/constraint/manager/VxConstraintManager.java
+++ common/src/main/java/net/xmx/velthoric/core/constraint/manager/VxConstraintManager.java
@@ -291,6 +291,16 @@ public class VxConstraintManager {
         return activeConstraints.containsKey(id);
     }
 
+    /**
+     * Retrieves active constraint by given ID.
+     * @param id The UUID of the constraint to retrieve.
+     * @return the constraint, or {@code null} if constraint is not found
+     */
+    @Nullable
+    public VxConstraint getActiveConstraint(UUID id) {
+        return activeConstraints.get(id);
+    }
+
     public VxConstraintStorage getConstraintStorage() {
         return constraintStorage;
     }
